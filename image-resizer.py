71e8c267a25d64c8374a6c4831e1f4a9f528e0d1
ankit-yadav-14 <166833442+ankit-yadav-14@users.noreply.github.com>
2024-04-13 15:59:01 +0530

    Add files via upload

---
+++ image-resizer.py
@@ -0,0 +1,22 @@
+import cv2
+
+# cofigurable parameters
+source = "image.jpg"
+destination = 'newimage.png'
+scale_percent = 50
+
+src = cv2.imread( source, cv2.IMREAD_UNCHANGED)
+# cv2.imshow("title", image)
+
+# percentage by which the image is resized
+
+# calculate the 50 percent of original dimension
+new_width = int(src.shape[1] * scale_percent/100)
+new_height = int(src.shape[0] * scale_percent/100)
+
+# resize image
+output = cv2.resize(src, (new_width, new_height))
+
+cv2.imwrite( destination,output)
+cv2.waitKey(0)
+
